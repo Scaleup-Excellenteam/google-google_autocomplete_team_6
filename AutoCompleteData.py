6fe6e5357f183dac785fd3ab3c9c78679bc1c991
OriVered <ori.vered@gmail.com>
2023-08-24 14:15:50 +0300

    Beta version work of naive and wrong in[it, need improve search and store time

--- AutoCompleteData.py
+++ AutoCompleteData.py
@@ -8,3 +8,8 @@ class AutoCompleteData:
     source_text: str
     offset: int
     score: int
+
+
+    def __str__(self):
+        return f"c'{self.completed_sentence}', ('{self.source_text}'{self.offset}) , score={self.score}"
+        #return f"completed_sentence='{self.completed_sentence}', source_text='{self.source_text}', offset={self.offset}, score={self.score}"
--- Automatic_sentence_completion.py
+++ Automatic_sentence_completion.py
@@ -1,17 +1,16 @@
 # coding: utf-8
 
 import os
-
+from typing import List
+from Levenshtein import editops
 from trie import Trie, TrieNode
 from AutoCompleteData import AutoCompleteData
 
 # Consts
 DATA_SUB_DIR = 'data'
 TXT_EXTENSION = '.txt'
-MAX_SCORE = 5
-SWAP_CHAR_SCORE = 1
-ADD_REMOVE_CHAR_SCORE = 2
-
+REPLACE_SCORE = [5, 4, 3, 2, 1]
+INSERT_DELETE_SCORE = [10, 8, 6, 4, 2]
 # Define the path relative to the current script's directory.
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIRECTORY = os.path.join(BASE_DIR, DATA_SUB_DIR)
@@ -29,10 +28,11 @@ def init(data_directory):
                         cleaned_line = line.strip()
                         words = cleaned_line.split()
                         for word_index, word in enumerate(words):
-                            cleaned_word = ''.join([char.lower() if char.isalpha() else char for char in word if char.isalpha() or char == ' '])
+                            cleaned_word = ''.join([char.lower() if char.isalpha() else char for char in word if
+                                                    char.isalpha() or char == ' '])
                             trie.insert(cleaned_word, {
                                 "line_number": line_num,
-                                "file_name": file,
+                                "file_name": file.split(".")[0],
                                 "line_content": cleaned_line,
                                 "word_index": word_index
                             })
@@ -70,24 +70,22 @@ def check_sequence_match(trie, current_match, words, mismatched, start_index):
     return True  # Sequence match found
 
 
-def search_trie(trie,sentence):
+def search_trie(trie, sentence):
     words = sentence.lower().split()
     mismatched = False
     start_index = 1
     first_word_matches = trie.search(words[0])
+
+    # if not found any word as first input word, mark as first word mismatched and continue with second word
     if not first_word_matches:
         first_word_matches = trie.search(words[1])
         mismatched = True
         start_index = 2
 
-    guessed_sentence = [match for match in first_word_matches if check_sequence_match(trie, match, words, mismatched, start_index)]
+    guessed_sentences = [match for match in first_word_matches if
+                         check_sequence_match(trie, match, words, mismatched, start_index)]
 
-    return guessed_sentence
-
-
-def complete_sentence(trie, user_input):
-    matches = search_trie(trie, user_input)
-    return matches
+    return guessed_sentences, mismatched, words, start_index
 
 
 def print_completions(completions):
@@ -98,10 +96,52 @@ def print_completions(completions):
     else:
         print("\nNo matches found for your input.")
 
-def compute_score(index, score):
-    if index < MAX_SCORE:
-        return abs(index - MAX_SCORE) * score
-    return 1
+
+def get_offset(line_content, word_index, words, start_index):
+    """Compute the offset where the input_sentence starts in line_content."""
+    content_words = line_content.lower().split()
+    start_word = content_words[word_index]
+    return line_content.lower().find(start_word)
+
+
+def get_score(line_content, input_sentence, offset):
+
+    line_guessed = line_content[offset:offset + len(input_sentence)]
+
+    # Exclude punctuation marks and calculate base score
+    base_score = sum(2 for char in input_sentence if char.isalnum())
+    score = base_score
+
+    edits = editops(line_guessed, input_sentence)
+
+    for op, _, pos in edits:
+        if op == "replace":
+            deduction = REPLACE_SCORE[min(pos, 4)]  # We use min to ensure that we never go out of the list bounds
+        else:  # for both insert and delete
+            deduction = INSERT_DELETE_SCORE[min(pos, 4)]
+
+        score -= deduction
+
+    return score
+
+
+def get_best_kֵ_completions(trie, user_input) -> List[AutoCompleteData]:
+    guessed_sentences, mismatched, input_words, start_index = search_trie(trie, user_input)
+
+    # Create a list to hold potential results
+    potential_results = []
+
+    for guess in guessed_sentences:
+        offset = get_offset(guess['line_content'], guess['word_index'], input_words, start_index)
+        mismatched_word = None if not mismatched else input_words[start_index - 1]
+        score = get_score(guess['line_content'], user_input, offset)
+        auto_data = AutoCompleteData(guess['line_content'], guess['file_name'], offset, score)
+        potential_results.append(auto_data)
+
+    # Sort the potential results by score in descending order
+    sorted_results = sorted(potential_results, key=lambda x: x.score, reverse=True)
+
+    return sorted_results
 
 
 def main():
@@ -110,9 +150,10 @@ def main():
     print("end read")
     user_input = input("Please enter a sentence for completion: ")
 
-    completions = complete_sentence(data_trie, user_input)
-    print(completions)
-   # print_completions(completions)
+    completions = get_best_kֵ_completions(data_trie, user_input)
+    for completion in completions:
+        print(completion)
+
 
 if __name__ == '__main__':
     main()
